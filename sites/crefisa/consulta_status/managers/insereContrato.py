4c3d310f0786a0ebfc574f2ca7e8a54f5893e9a7
Marcelo Amancio <marcelo@brius.com.br>
2024-05-07 10:20:48 -0300

    ajsute insercao contrato no system exceptio

--- sites/crefisa/consulta_status/managers/insereContrato.py
+++ sites/crefisa/consulta_status/managers/insereContrato.py
@@ -118,6 +118,8 @@ class InserirContrato(Manager):
 
             if retorno_mensagem != "":
                 dados_atualizacao['mensagem'] = 'Reprovado a Conferir'
+                if('System.Exception' in retorno_mensagem):
+                    dados_atualizacao['mensagem'] = 'Conferir dados do contrato'
                 dados_atualizacao['erro'] = retorno_mensagem
                 dados_atualizacao['observacao'] = retorno_mensagem
                 self.atualiza.atualizar_contrato(contrato['codigo_con'], dados_atualizacao)
@@ -294,6 +296,12 @@ class InserirContrato(Manager):
                 dados_atualizacao['mensagem'] = 'Pendente Dados'
                 dados_atualizacao['textoMensagem'] = retorno['mensagem']
                 dados_atualizacao['observacao'] = retorno['mensagem']
+                if('System.Exception' in retorno_mensagem):
+                    dados_atualizacao['mensagem'] = 'Conferir dados do contrato'
+                    dados_atualizacao['interacaoHumana'] = 0
+                    dados_atualizacao['observacao'] = "Erro ao inserir: "+retorno['mensagem']
+                    dados_atualizacao['erro'] = retorno['mensagem']
+                
                 self.atualiza.atualizar_contrato(contrato['codigo_con'], dados_atualizacao)
                 self.remove_div()
                 continue
