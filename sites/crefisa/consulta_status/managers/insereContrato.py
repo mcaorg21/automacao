8ae99f71a171802345c473eef03c0f5aad69fbee
Marcelo Amancio <marcelo@brius.com.br>
2024-11-26 16:17:32 -0300

    ajuste de insercao

--- sites/crefisa/consulta_status/managers/insereContrato.py
+++ sites/crefisa/consulta_status/managers/insereContrato.py
@@ -639,11 +639,25 @@ class InserirContrato(Manager):
 
                 if(novo_contrato == True):
                     try:
+
                         self.act.select_drop_down('//*[@id="txtTipoValorContrato"]','1', By.XPATH)
                     except:
+
                         self.act.select_drop_down("//select[@id='txtTipoSimulacao']",'1', By.XPATH)
                         self.act.select_drop_down('//*[@id="txtTipoValorContrato"]', '1', By.XPATH)
-                        pass
+                    finally:
+
+                        dados_atualizacao['mensagem'] = 'Reprovado a Conferir'
+                        dados_atualizacao['observacao_emp'] = retorno['mensagem'] + " E não há como realizar novo contrato."
+                        dados_atualizacao['observacao'] = retorno['mensagem'] + " E não há como realizar novo contrato."
+                        dados_atualizacao['erro'] = retorno['mensagem'] + " E não há como realizar novo contrato."
+                        dados_atualizacao['status_con'] = "Reprovado a Conferir"
+
+                        self.atualiza.atualizar_contrato(contrato['codigo_con'], dados_atualizacao)
+
+                        print("XXXXXXXXXXXXXXX NÃO PODE REALIZAR NOVO OU REFIN XXXXXXXXXXXXXXXXX")
+                        continue
+
 
                     self.act.enviar_texto('//*[@id="txtValorSimulacao"]', informacoes['contrato']['valorParcela'], By.XPATH) 
                     print('----------------------------------------------------------------------------------------')
