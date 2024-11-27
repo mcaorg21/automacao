1a6a69d3dcee74624a6b781c568149f1369f37bd
Marcelo Amancio <marcelo@brius.com.br>
2024-11-27 13:04:37 -0300

    ajsute para insercao dar 3 minutos

--- sites/crefisa/consulta_status/managers/insereContrato.py
+++ sites/crefisa/consulta_status/managers/insereContrato.py
@@ -165,8 +165,8 @@ class InserirContrato(Manager):
                         dados_atualizacao['erro'] = retorno['mensagem']
 
                     if('Experimente fazer login novamente' in retorno_mensagem or 'Erro interno' in retorno_mensagem):
-                        print('XXXXXXXXXXX Sessão deslogada... Aguardando 15 s pra logar XXXXXXXXXXX')
-                        time.sleep(15)
+                        print('XXXXXXXXXXX Sessão deslogada... Aguardando 180 s pra logar XXXXXXXXXXX')
+                        time.sleep(180)
                         self.driver.delete_all_cookies()
                         self.driver.quit()
                         continue
